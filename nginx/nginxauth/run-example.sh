b8688e9dfd7c5ed7c3d637edbf4425cbd35bfe49
jnbnyc <jnbuhaynyc@gmail.com>
2015-08-07 23:27:47 -0400

    fixing up single container

--- nginx/nginxauth/run-example.sh
+++ nginx/nginxauth/run-example.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# docker build -t python-dep:2.7 -f Dockerfile.python-dep .
 # docker build -t backend -f Dockerfile.backend .
-docker build -t nginx:auth -f Dockerfile.nginx .
+# docker build -t ldapauthd -f Dockerfile.ldapauthd .
+# docker build -t nginx:auth -f Dockerfile.nginx .
+docker build -t nginx:all -f Dockerfile.all .
 
 docker run -d --name ldap -p 389:389 -p 636:636 osixia/openldap
-docker run -d --name auth -p 80:8081 nginx:auth
+# docker run -d --name backend -p 9000:9000 backend
+# docker run -d --name ldapauth -p 8888:8888 ldapauthd
+# docker run -d --name nginx -p 80:8081 nginx:auth
 
-docker exec auth python backend-sample-app.py &
-docker exec auth ./nginx-ldap-auth-daemon-ctl.sh start
-docker exec auth ps aux
+docker run -d --name auth -p 80:8081 nginx:all
+
+# docker exec auth python backend-sample-app.py &
+# docker exec auth ./nginx-ldap-auth-daemon-ctl.sh start
+# docker exec auth ps aux
 # docker exec ldap ldapsearch -x -h localhost -b dc=example,dc=org -D "cn=admin,dc=example,dc=org" -w admin
 
 curl -i http://192.168.59.103/
