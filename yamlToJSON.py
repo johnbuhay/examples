7cedd2d0426598bd879611269417798b5a284de6
John Buhay <john.buhay@wework.com>
2016-11-26 16:00:10 -0500

    Remove yamlToJSON file

--- yamlToJSON.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
- 
-import simplejson
-import sys
-import yaml
-  
-print simplejson.dumps(yaml.load(str(sys.stdin.read())), sort_keys=True)
-#python -c 'import simplejson,sys,yaml;print simplejson.dumps(yaml.load(str(sys.stdin.read())), sort_keys=True)'
